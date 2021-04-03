13ed4047febb6fe741968946f81a56fdd2b00535
Matthew Ward <mattrward1030@gmail.com>
2021-04-03 10:15:14 -0400

    starting to write get requests for the notes.html and index.html, now working on getting my api notes reading the db.json file

--- public/assets/JS/server.js
+++ public/assets/JS/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
+
+const data = fs.readFileSync('db.json');
+const notes = JSON.parse(data);
+
 
 // Sets up the Express App
 
@@ -9,3 +15,29 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+// route that sends user to the notes.hmtl page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+// route that sends user to the index.html page
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+// setting up api get requests
+app.get('/api/notes', (req, res) => res.json(notes));
+
+
+
+
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
