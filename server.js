4a08fa16aae738b3f2ef3e4e92c0b7e0ba80dec2
Matthew Ward <mattrward1030@gmail.com>
2021-04-05 21:20:36 -0400

    trying to get styles to work with heroku, working locally

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use('styles.css', (req, res) => {
+    res.sendFile('styles.css');
+});
 
 
 
