9acbbc9ff7363ef4c1dfa1b46a99c73cab102034
Matthew Ward <mattrward1030@gmail.com>
2021-04-05 21:23:20 -0400

    trying to solve heroku issues. changed * to / in index.html path

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.delete("/api/notes/:id", function (req, res) {
 });
 
 // route that sends user to the index.html page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
