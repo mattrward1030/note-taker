e00e9bb0101ba9658e136b63093e39c9eaf3efca
Matthew Ward <mattrward1030@gmail.com>
2021-04-05 21:24:47 -0400

    fixing issues

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use('styles.css', (req, res) => {
 });
 
 
-
+// route that sends user to the index.html page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 // route that sends user to the notes.hmtl page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
@@ -85,8 +86,7 @@ app.delete("/api/notes/:id", function (req, res) {
     res.json(notesDatabase);
 });
 
-// route that sends user to the index.html page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
